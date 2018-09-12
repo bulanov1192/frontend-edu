8e94f9e20574efcdfe43ffd7fea0bed8b8384a5e
bulanov1192 <bulanov1192@gmail.com>
2018-09-12 15:56:22 +0500

    clear officeBranch

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
-
-gulp.task('sass', function () {
-   return gulp.src('app/scss/main.sass')
-       .pipe(sass())
-       .pipe(gulp.dest('dist/css'))
-});
